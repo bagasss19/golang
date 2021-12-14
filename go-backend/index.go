ba588d343bd34c6c077ba4bb2d71220415d84684
bagasss19 <bagasgustip19@gmail.com>
2021-12-14 21:50:52 +0700

    update

--- go-backend/index.go
+++ go-backend/index.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	guuid "github.com/google/uuid"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
@@ -17,6 +22,82 @@ type User struct {
 	Email    string `json:"email" binding:"required"`
 	Password string `json:"password" binding:"required"`
 	Username string `json:"username" binding:"required"`
+}
+
+type Login struct {
+	Password string `json:"password" binding:"required"`
+	Email    string `json:"email" binding:"required"`
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func CreateToken(userId string) (string, error) {
+	var err error
+	//Creating Access Token
+	os.Setenv("ACCESS_SECRET", "jdnfksdmfksd") //this should be in an env file
+	atClaims := jwt.MapClaims{}
+	atClaims["authorized"] = true
+	atClaims["user_id"] = userId
+	// atClaims["exp"] = time.Now().Add(time.M * 15).Unix()
+	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
+	token, err := at.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
+	if err != nil {
+		return "", err
+	}
+	return token, nil
+}
+
+func LoginUser(c *gin.Context) {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx := context.Background()
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+
+	collection := client.Database("backend").Collection("user")
+	var u Login
+	if err := c.ShouldBindJSON(&u); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, "Invalid json provided")
+		return
+	}
+	user := User{}
+	error := collection.FindOne(context.TODO(), bson.M{"email": u.Email}).Decode(&user)
+	if error != nil {
+		log.Printf("Wrong Email or Password %v\n", error)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status":  http.StatusInternalServerError,
+			"message": "Wrong Email or Password",
+		})
+		return
+	}
+
+	match := CheckPasswordHash(u.Password, user.Password)
+	if !match {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status":  http.StatusInternalServerError,
+			"message": "Wrong Email or Password",
+		})
+	}
+
+	token, err := CreateToken(u.Email)
+	if err != nil {
+		c.JSON(http.StatusUnprocessableEntity, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, token)
 }
 
 func getUser(c *gin.Context) {
@@ -35,7 +116,7 @@ func getUser(c *gin.Context) {
 	collection := client.Database("backend").Collection("user")
 	cur, err := collection.Find(context.TODO(), bson.M{})
 	if err != nil {
-		log.Printf("Error while getting all todos, Reason: %v\n", err)
+		log.Printf("Error while getting all users, Reason: %v\n", err)
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"status":  http.StatusInternalServerError,
 			"message": "Something went wrong",
@@ -50,9 +131,8 @@ func getUser(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{
-		"status":  http.StatusOK,
-		"message": "All User",
-		"data":    users,
+		"status": http.StatusOK,
+		"data":   users,
 	})
 	return
 }
@@ -73,7 +153,7 @@ func createUser(c *gin.Context) {
 	var user User
 	c.BindJSON(&user)
 	email := user.Email
-	password := user.Password
+	password, _ := HashPassword(user.Password)
 	username := user.Username
 	id := guuid.New().String()
 
@@ -87,7 +167,7 @@ func createUser(c *gin.Context) {
 	_, error := collection.InsertOne(context.TODO(), newUser)
 
 	if error != nil {
-		log.Printf("Error while inserting new todo into db, Reason: %v\n", err)
+		log.Printf("Error while inserting new user into db, Reason: %v\n", err)
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"status":  http.StatusInternalServerError,
 			"message": "Something went wrong",
@@ -97,7 +177,48 @@ func createUser(c *gin.Context) {
 
 	c.JSON(http.StatusCreated, gin.H{
 		"status":  http.StatusCreated,
-		"message": "Todo created Successfully",
+		"message": "User created Successfully",
+	})
+	return
+}
+
+func editUser(c *gin.Context) {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx := context.Background()
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+
+	collection := client.Database("backend").Collection("user")
+
+	id := c.Param("id")
+	var user User
+	c.BindJSON(&user)
+
+	newData := bson.M{
+		"$set": bson.M{
+			"username": user.Username,
+		},
+	}
+
+	_, error := collection.UpdateOne(context.TODO(), bson.M{"id": id}, newData)
+	if error != nil {
+		log.Printf("Error, Reason: %v\n", error)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status":  500,
+			"message": "Something went wrong",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status":  200,
+		"message": "User Edited Successfully",
 	})
 	return
 }
@@ -117,7 +238,7 @@ func deleteUser(c *gin.Context) {
 	collection := client.Database("backend").Collection("user")
 	_, error := collection.DeleteOne(context.TODO(), bson.M{"id": c.Param("id")})
 	if error != nil {
-		log.Printf("Error while deleting a single todo, Reason: %v\n", err)
+		log.Printf("Error while deleting a single user, Reason: %v\n", err)
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"status":  http.StatusInternalServerError,
 			"message": "Something went wrong",
@@ -127,15 +248,100 @@ func deleteUser(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{
 		"status":  http.StatusOK,
-		"message": "Todo deleted successfully",
+		"message": "User deleted successfully",
 	})
 	return
 }
 
+func getSingleUser(c *gin.Context) {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx := context.Background()
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+
+	collection := client.Database("backend").Collection("user")
+
+	id := c.Param("id")
+
+	user := User{}
+	error := collection.FindOne(context.TODO(), bson.M{"id": id}).Decode(&user)
+	if error != nil {
+		log.Printf("Error while getting a single User, Reason: %v\n", err)
+		c.JSON(http.StatusNotFound, gin.H{
+			"status":  http.StatusNotFound,
+			"message": "User not found",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status": http.StatusOK,
+		"data":   user,
+	})
+	return
+}
+
+func ExtractToken(r *http.Request) string {
+	bearToken := r.Header.Get("Authorization")
+	//normally Authorization the_token_xxx
+	strArr := strings.Split(bearToken, " ")
+	if len(strArr) == 2 {
+		return strArr[1]
+	}
+	return ""
+}
+
+func VerifyToken(r *http.Request) (*jwt.Token, error) {
+	tokenString := ExtractToken(r)
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		//Make sure that the token method conform to "SigningMethodHMAC"
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("ACCESS_SECRET")), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return token, nil
+}
+
+func TokenValid(r *http.Request) error {
+	token, err := VerifyToken(r)
+	if err != nil {
+		return err
+	}
+	if _, ok := token.Claims.(jwt.Claims); !ok && !token.Valid {
+		return err
+	}
+	return nil
+}
+
+func TokenAuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		err := TokenValid(c.Request)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, err.Error())
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
+
 func main() {
 	r := gin.Default()
-	r.GET("/user", getUser)
-	r.POST("/user", createUser)
-	r.DELETE("/user/:id", deleteUser)
+	r.GET("/user", TokenAuthMiddleware(), getUser)
+	r.GET("/user/:id", TokenAuthMiddleware(), getSingleUser)
+	r.POST("/user", TokenAuthMiddleware(), createUser)
+	r.POST("/login", LoginUser)
+	r.PUT("/user/:id", TokenAuthMiddleware(), editUser)
+	r.DELETE("/user/:id", TokenAuthMiddleware(), deleteUser)
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
